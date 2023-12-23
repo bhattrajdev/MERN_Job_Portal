62dfcfd4d2ce15c15704519b2838803ff6fefdf3
bhattrajdev <devrajbhatt010@gmail.com>
2023-12-23 09:20:37 +0545

    user auth

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,25 +1,31 @@
 import express from "express";
-import User from "../middleware/models/UserModel.js";
+import User from "../models/userModel.js";
+import generateToken from "../utils/generateToken.js";
 
 // FOR CREATING A NEW USER
 const createUser = async (req, res) => {
   try {
     const { name, email, password } = req.body;
-    const user = await User.create({
-      name,
-      email,
-      password,
-    });
+    const userAlreadyExists = await User.findOne({ email });
+    if (userAlreadyExists) {
+      res.status(500).json({ error: "User Already Exists" });
+    } else {
+      const user = await User.create({
+        name,
+        email,
+        password,
+      });
 
-    res.status(201).json({ user });
+      res.status(201).json({ user });
+    }
   } catch (error) {
     console.error("Error creating user:", error);
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
-// FOR GETTING ALL USER DATA
-const getUsers = async (req, res, next) => {
+// FOR GETTING ALL USERS DATA
+const getUsers = async (req, res) => {
   try {
     const users = await User.find();
     res.json({ users });
@@ -29,57 +35,82 @@ const getUsers = async (req, res, next) => {
   }
 };
 
-// FOR GETTING A USER PROFILE
-const getUser = async (req, res, next) => {
+// FOR GETTING A USER DATA
+const getUser = async (req, res) => {
   try {
     const user = await User.findById(req.params.id);
     if (user) {
-      res.status(400)
-      res.json({user})
+      res.status(400);
+      res.json({ user });
     }
   } catch (error) {
-  res.status(500)
-  throw new Error("User Not Found")
+    res.status(500);
+    throw new Error("User Not Found");
   }
 };
 
 // TO DELETE A USER PROFILE
-const deleteUser = async (req,res,next)=>{
-  try{
+const deleteUser = async (req, res) => {
+  try {
     const user = await User.findById(req.params.id);
-    if(user){
-      await User.deleteOne()
-      res.json({message: "User deleted successfully"})
-    }else{
-      res.status(500)
-      throw new Error("User not found")
+    if (user) {
+      await User.deleteOne();
+      res.json({ message: "User deleted successfully" });
+    } else {
+      res.status(500);
+      throw new Error("User not found");
     }
-  }catch(error){
-    res.status(500)
+  } catch (error) {
+    res.status(500);
     throw new Error("Error while delting the user");
   }
-}
+};
 
 // TO UPDATE A USER PROFILE
-const updateUser = async (req,res,next)=>{
-  try{
-    const user = await User.findById(req.params.id);
-    if(user){
-      user.name = req.body.name
-      user.email = req.body.email
-      user.password = req.body.password
-      const updateUser = await user.save()
-      
-      res.json({user})
-    }else{
-      res.status (404)
-      throw new Error ("User not found")
+const updateUser = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const user = await User.findById(id);
+    if (user) {
+      user.name = req.body.name;
+      user.email = req.body.email;
+      user.password = req.body.password;
+      const updateUser = await user.save();
+
+      res.json({
+        _id: updateUser._id,
+        name: updateUser.name,
+        email: updateUser.email,
+        isAdmin: updateUser.isAdmin,
+        token: generateToken(id),
+      });
+    } else {
+      res.status(404);
+      throw new Error("User not found");
     }
-  }catch(error){
-    res.status(500)
-    throw new Error("Cannot Update the user")
+  } catch (error) {
+    res.status(500);
+    throw new Error("Cannot Update the user");
   }
-}
+};
 
+// TO AUTHENTICATE A USER
+const authUser = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
 
-export { createUser, getUsers ,getUser,deleteUser,updateUser};
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
+};
+
+export { createUser, getUsers, getUser, deleteUser, updateUser, authUser };
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -4,6 +4,7 @@ import {
   getUser,
   deleteUser,
   updateUser,
+  authUser,
 } from "../controllers/UserController.js";
 import express from "express";
 
@@ -12,5 +13,5 @@ const router = express.Router();
 // routers realted to users
 router.route("/").get(getUsers).post(createUser).get(getUser);
 router.route("/:id").get(getUser).delete(deleteUser).put(updateUser);
-
+router.route("/login").post(authUser)
 export default router;
